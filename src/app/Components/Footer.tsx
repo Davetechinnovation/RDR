6dd0be4384f2d6da98fc5149f15c380394e3fd59
Danraph-Technologies <davetechinnovation@gmail.com>
2025-10-16 15:34:41 +0100

    feat(Components): add Footer component and integrate into layout
    - Create Footer component with branding, social media icons, and company message
    - Add Tailwind CSS classes for styling
    - Import and include Footer in the main layout

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className='bg-[#232323]'>
+    <div className="bg-[#232323] p-5 text-white ">
+      <img src="/rdr logo.webp" alt="" />
+      <div>
         <div>
-            <div>
-                <img src="/rdr-logo" alt="" />
-            </div>
+          <p>
+            At RDR General Construction, Building Safe, Strong, and Sustainable
+            Spaces is more than a tagline—it’s the foundation of everything we
+            do. We believe every project should stand the test of time
+          </p>
+          <div className="bg-[#DF383D] w-[48px] h-[48px] rounded-full flex justify-center items-center ">
+            <img src="/facebook.png" alt="" className="w-[19px]" />
+          </div>
+          <div>
+            <img src="/x.png" alt="" className="w-[17px]" />
+          </div>
+
+          <img src="/linkedin.png" alt="" className="w-[17px]" />
+          <img src="/instagram.png" alt="" className="w-[17px]" />
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
