e024a4e6af95e54c8e8d9004c451892299df9bea
Danraph-Technologies <davetechinnovation@gmail.com>
2025-10-16 12:17:40 +0100

    feat(Footer): add RDR logo to footer component
    - Fix background color syntax in Footer component
    - Add nested div structure for logo placement
    - Include img element for RDR logo

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-#232323]'>
+    <div className='bg-[#232323]'>
+        <div>
+            <div>
+                <img src="/rdr-logo" alt="" />
+            </div>
+        </div>
       
     </div>
   )
