29019491ae968d1f824548daa47d936dbea0cbc4
Danraph-Technologies <davetechinnovation@gmail.com>
2025-10-29 13:40:21 +0100

    feat(Footer): add hyperlink to RDR logo
    - Wrap the RDR logo SVG with an anchor tag
    - Set the href attribute to "www.google.com" (placeholder URL)
    - Maintain the existing logo styling and positioning

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -9,23 +9,25 @@ function Footer() {
         <div>
           <p className="max-w-[412px] py-5 text-[14px] sm:text-[16px] ">
             At RDR General Construction, Building Safe, Strong, and Sustainable
-            Spaces is more than a tagline it’s the foundation of everything we
+            Spaces is more than a tagline it's the foundation of everything we
             do. We believe every project should stand the test of time
           </p>
           <div className="flex gap-2 items-center">
             <div className="bg-[#DF383D] sm:w-[48px] sm:h-[48px] w-[38px] h-[38px] rounded-full flex justify-center items-center ">
-              <svg
-                width="14"
-                height="14"
-                viewBox="0 0 14 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12.5 0C13.3125 0 14 0.6875 14 1.5V12.5C14 13.3438 13.3125 14 12.5 14H8.1875V9.25H10L10.3438 7H8.1875V5.5625C8.1875 4.9375 8.5 4.34375 9.46875 4.34375H10.4375V2.4375C10.4375 2.4375 9.5625 2.28125 8.6875 2.28125C6.9375 2.28125 5.78125 3.375 5.78125 5.3125V7H3.8125V9.25H5.78125V14H1.5C0.65625 14 0 13.3438 0 12.5V1.5C0 0.6875 0.65625 0 1.5 0H12.5Z"
-                  fill="white"
-                />
-              </svg>
+              <a href="www.google.com">
+                <svg
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12.5 0C13.3125 0 14 0.6875 14 1.5V12.5C14 13.3438 13.3125 14 12.5 14H8.1875V9.25H10L10.3438 7H8.1875V5.5625C8.1875 4.9375 8.5 4.34375 9.46875 4.34375H10.4375V2.4375C10.4375 2.4375 9.5625 2.28125 8.6875 2.28125C6.9375 2.28125 5.78125 3.375 5.78125 5.3125V7H3.8125V9.25H5.78125V14H1.5C0.65625 14 0 13.3438 0 12.5V1.5C0 0.6875 0.65625 0 1.5 0H12.5Z"
+                    fill="white"
+                  />
+                </svg>
+              </a>
             </div>
             <div className="bg-[#DF383D] sm:w-[48px] sm:h-[48px] w-[38px] h-[38px] rounded-full flex justify-center items-center ">
               <svg
